d7c7c62b624d70d523f0a8972c0176c15e62e616
Aman Chandel <amaniiitu32@gmail.com>
2022-04-17 11:18:55 +0530

    portfolio

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React from 'react';
+import './body.css';
+import About from './about/About';
+import Projects from './projects/Projects';
+import Skills from './skills/Skills';
+import Works from './work/Works';
+import Contact from './contact/Contact';
 
 function Body() {
   return (
-    <div>Body</div>
+    <div className='body'>
+      <section id='about'>
+        <About/>
+      </section>
+      <section id='projects'>
+        <Projects/>
+      </section>
+      <section id='skills'>
+        <Skills/>
+      </section>
+      <section id='work'>
+        <Works/>
+      </section>
+      <section id='contact'>
+        <Contact/>
+      </section>
+    </div>
   )
 }
 
